2e7e6e884504e31ab936263b12e65be49c5138f3
npdwuy <huynguyen030707@gmail.com>
2026-07-25 19:57:12 +0700

    chore: remove all things involving framer package and config, keep framer-motion, clean up configs

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // Cho phép build production thành công ngay cả khi các file generated có lỗi type
-    ignoreBuildErrors: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
